f41e493321422593380be235a55e3250da58fc3e
Eric <unbirdlikebird@hotmail.com>
2015-05-15 09:49:49 +0800

    Initial Commit

---
+++ 15052015@keyboardTests/_5052015_keyboardTests.m
@@ -0,0 +1,34 @@
+//
+//  _5052015_keyboardTests.m
+//  15052015@keyboardTests
+//
+//  Created by Henry on 15-5-15.
+//  Copyright (c) 2015年 unbirdlikebird. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _5052015_keyboardTests : XCTestCase
+
+@end
+
+@implementation _5052015_keyboardTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
